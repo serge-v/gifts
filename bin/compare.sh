b877abc9597072d241e51df1661edaa293497ff1
Serge Voil <serge0x76@gmail.com>
2014-12-02 23:13:22 -0500

    using password manager

--- bin/compare.sh
+++ bin/compare.sh
@@ -1,3 +1,3 @@
 [[ ! -d prod~ ]] && mkdir prod~
-ncftpget -u voilokov -p `pass gifts` ftp.voilokov.com prod~/ '/public_html/gifts/trunk/*.*'
+ncftpget -R -u voilokov -p `pass gifts` ftp.voilokov.com prod~/ '/public_html/gifts/trunk/*.*'
 diff -q src/ prod~/
