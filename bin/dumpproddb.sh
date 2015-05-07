2a1b18f546428c9b0b547ee4ebf335fa8a6d2e2f
Serge Voilokov <svoilokov@appnexus.com>
2015-05-07 09:11:59 -0400

    mysql bin path added

--- bin/dumpproddb.sh
+++ bin/dumpproddb.sh
@@ -1,2 +1,4 @@
+export PATH=/opt/mysql/bin:$PATH
+
 mysqldump voilokov_gifts -h voilokov.com --no-data -u voilokov_dbadmin --password=`pass gifts-db` > db_schema.txt
 mysqldump voilokov_gifts -h voilokov.com -u voilokov_dbadmin --password=`pass gifts-db` > db_data.sql
